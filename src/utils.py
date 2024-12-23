22ce1f5167436a8d9293ead7bfe1bf0ee5ebb6d2
LikhitaChilla <chillalikhita1242@gmail.com>
2024-12-23 11:34:26 +0530

    hello

--- src/utils.py
+++ src/utils.py
@@ -1,11 +1,9 @@
-import logging 
 import os
 import sys
 
 import numpy as np 
 import pandas as pd
 import dill
-import pickle
 from sklearn.metrics import r2_score
 from sklearn.model_selection import GridSearchCV
 
@@ -54,10 +52,10 @@ def evaluate_models(X_train, y_train,X_test,y_test,models):
     except Exception as e:
         raise CustomException(e, sys)
     
-# def load_object(file_path):
-#     try:
-#         with open(file_path, "rb") as file_obj:
-#             return pickle.load(file_obj)
+def load_object(file_path):
+    try:
+        with open(file_path, "rb") as file_obj:
+            return dill.load(file_obj)
 
-#     except Exception as e:
-#         raise CustomException(e, sys)
+    except Exception as e:
+        raise CustomException(e, sys)
